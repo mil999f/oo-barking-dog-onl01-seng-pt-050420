7f93440489f0929560a1357882ca4793c027ce12
Milly Flores <miflo0h@gmail.com>
2020-05-21 05:01:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Dog 
+  
+  def get_name 
+    return @name
+  end
+  
+  def set_name(dog_name)
+    @name = dog_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end
+
+dog1 = 
